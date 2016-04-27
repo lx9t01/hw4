63c8c48481e84900bfbae23b1ebc77089b87774d
lx9t01 <alex.xuhaixiang@gmail.com>
2016-04-27 02:51:38 -0700

    with filter

--- xray_ct_recon.cu
+++ xray_ct_recon.cu
@@ -60,15 +60,15 @@ void cudaMultiplyKernel(cufftComplex *raw_data,
     unsigned int thread_index = blockIdx.x * blockDim.x + threadIdx.x;
 
     while (thread_index < nAngles * sinogram_width) {
-        // unsigned int p = thread_index % sinogram_width; 
-        // if (p < sinogram_width / 2) {
-        //     raw_data[thread_index].x = raw_data[thread_index].x * (2.0 * p / sinogram_width);
-        //     raw_data[thread_index].y = raw_data[thread_index].y * (2.0 * p / sinogram_width);
-        // } else {
-        //     raw_data[thread_index].x = raw_data[thread_index].x * (2.0 * (sinogram_width - p) / sinogram_width);
-        //     raw_data[thread_index].y = raw_data[thread_index].y * (2.0 * (sinogram_width - p) / sinogram_width);
+        unsigned int p = thread_index % sinogram_width; 
+        if (p < sinogram_width / 2) {
+            raw_data[thread_index].x = raw_data[thread_index].x * (2.0 * p / sinogram_width);
+            raw_data[thread_index].y = raw_data[thread_index].y * (2.0 * p / sinogram_width);
+        } else {
+            raw_data[thread_index].x = raw_data[thread_index].x * (2.0 * (sinogram_width - p) / sinogram_width);
+            raw_data[thread_index].y = raw_data[thread_index].y * (2.0 * (sinogram_width - p) / sinogram_width);
 
-        // }
+        }
         
         // raw_data[thread_index].x = raw_data[thread_index].x;
         
