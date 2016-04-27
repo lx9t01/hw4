17e88f8663f7580760b3d7b2681e6743746d7638
lx9t01 <alex.xuhaixiang@gmail.com>
2016-04-26 22:23:57 -0700

    gloabl2

--- xray_ct_recon.cu
+++ xray_ct_recon.cu
@@ -54,7 +54,7 @@ void checkCUDAKernelError()
 
 }
 
-__gloabl__ 
+__global__ 
 void cudaMultiplyKernel(const cufftComplex *raw_data, const cufftComplex *impulse_v, 
                                 cufftComplex *out_data, unsigned int nAngles, unsigned int sinogram_width) {
     unsigned int l = nAngles * sinogram_width; 
